a2d99e45593c48cd7c01bc4a6c173ec0740a0713
Justin Klitgaard <jlke45@gmial.com>
2024-05-30 15:06:30 +0900

    Change to placeholder content

--- src/pages/NaganoCity.jsx
+++ src/pages/NaganoCity.jsx
@@ -3,10 +3,7 @@ import data from "../data/data.json";
 const NaganoCity = () => {
   return (
     <div>
-      <h1>{data.naganoCity.name}</h1>
-      <p>{data.naganoCity.description}</p>
-      <button>{data.naganoCity.options[1]}</button>
-      <button>{data.naganoCity.options[2]}</button>
+      <h1>Nagano City</h1>
     </div>
   );
 };
