49687a8c9e2b5b1a998283ae6231aef2c59a46bd
Justin Klitgaard <jlke45@gmial.com>
2024-05-30 20:03:01 +0900

    Increase box shadow size

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -5,14 +5,14 @@ import arukuma from "../assets/arukuma.png";
 const Top = () => {
   return (
     <div className="bg-green-400 min-h-screen">
-      <div className="bg-cyan-300 w-4/5 min-h-screen mx-auto shadow-md flex flex-col justify-around items-center pb-24">
-        <div className="bg-white rounded shadow">
-          <h1 className="text-4xl p-2 text-green-950">Nagano Quest</h1>
+      <div className="bg-cyan-300 w-4/5 min-h-screen mx-auto shadow-lg flex flex-col justify-around items-center pb-24">
+        <div className="bg-white rounded shadow-lg">
+          <h1 className="text-4xl p-3 text-green-950">Nagano Quest!</h1>
         </div>
         <div className="flex justify-center">
           <img src={arukuma} alt="arukuma" className="size-3/4" />
         </div>
-        <button className="bg-white rounded shadow text-3xl p-2 text-green-950">
+        <button className="bg-white rounded shadow-lg text-3xl p-2 text-green-950">
           <Link to="naganocity">Start</Link>
         </button>
       </div>
